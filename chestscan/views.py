3a2edd1d24d875785f511907e4e1c6bd1f86b60b
Abhay Nigam <mashuk1998@gmail.com>
2020-05-02 19:47:00 +0530

    final

--- chestscan/views.py
+++ chestscan/views.py
@@ -37,7 +37,7 @@ def getClientResponse(request):
         # img = open_image(request.POST.get('image'))
         # pred_class,pred_idx,outputs = learn.predict(img)
         #data = [{'category': pred_class, 'tensors': outputs}]
-        print(request.data)
+        print(request.FILES)
         imgObj = serializer.RequestSerializer(data=request.data)
         #if imgObj.is_valid():
         
