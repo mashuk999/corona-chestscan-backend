67356f5bb9383e0ae2165fe17df0cf7e645e889d
Abhay Nigam <mashuk1998@gmail.com>
2020-05-02 15:50:24 +0530

    final

--- chestscan/views.py
+++ chestscan/views.py
@@ -9,8 +9,6 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
 
 # Create your views here.
 def homepage(request):
