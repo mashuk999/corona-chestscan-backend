c91322077e466a834b1f52eda9f3ef89d1b8303f
Abhay Nigam <mashuk1998@gmail.com>
2020-05-02 21:11:57 +0530

    final

--- chestscan/views.py
+++ chestscan/views.py
@@ -11,6 +11,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import io
 from . import models,serializer
+from django.core.files import File
 
 # Create your views here.
 def homepage(request):
